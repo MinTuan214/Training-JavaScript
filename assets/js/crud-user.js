fa991d0c72e0adee510fbc6d00000b9b331169c9
Tuấnu <minhtuan21102004@an21102004gmail.com>
2024-08-28 10:35:12 +0700

    update

--- assets/js/crud-user.js
+++ assets/js/crud-user.js
@@ -60,8 +60,8 @@ function showUsers(){
                     <td>${element.country}</td>
                     <td class="action">
                         <a href="${index}"><i class="fa-regular fa-eye"></i></a> |
-                        <button  onclick="updateUser()" class="btn-update"><i class="fa-regular fa-pen-to-square"></i></button> |
-                        <button onclick="deleteUser(${index})"><i class="fa-regular fa-trash-can"></i></button>
+                        <button onclick="updateUser(${index})" class="btn-update"><i class="fa-regular fa-pen-to-square"></i></button> |
+                        <button onclick="confirmDelete(${index})" class="btn-delete"><i class="fa-regular fa-trash-can"></i></button>
                     </td>
                 </tr>
         `
@@ -71,14 +71,6 @@ function showUsers(){
 
 
 document.onload = showUsers();
-
-// Create
-function createUser(index){
-    document.getElementById('save').style.display = 'block';
-    document.getElementById('title-add').style.display = 'block';
-    document.getElementById('update').style.display = 'none';
-    document.getElementById('title-update').style.display = 'none';
-}
 
 // Add user
 function addUser(){
@@ -116,6 +108,18 @@ function addUser(){
         var country = document.getElementById('country').value = "";
         var city = document.getElementById('city').value = "";
         var address = document.getElementById('address').value = "";
+
+    location.reload();
+
+    }
+
+}
+
+function confirmDelete(index) {
+    document.querySelector('.confirm-delete').classList.add('active');
+    document.querySelector('.delete').setAttribute('onclick', `deleteUser(${index})`);
+    document.querySelector('.cancel').onclick = function() {
+        document.querySelector('.confirm-delete').classList.remove('active');
     }
 }
 
@@ -130,7 +134,10 @@ function deleteUser(index){
 
     listUsers.splice(index, 1);
     localStorage.setItem('listUsers', JSON.stringify(listUsers));
+    document.querySelector('.confirm-delete').style.display = 'none';
     showUsers();
+    location.reload();
+
 }
 
 //update and edit
@@ -139,5 +146,35 @@ function updateUser(index){
     document.getElementById('title-add').style.display = 'none';
     document.getElementById('update').style.display = 'block';
     document.getElementById('title-update').style.display = 'block';
-    
+    var listUsers;
+        if(localStorage.getItem("listUsers") == null){
+            listUsers = []; 
+        }else{
+            listUsers = JSON.parse(localStorage.getItem("listUsers"));
+        }
+    document.getElementById('lastName').value = listUsers[index].lastName;
+    document.getElementById('firstName').value = listUsers[index].firstName;
+    document.getElementById('email').value = listUsers[index].email;
+    document.getElementById('phone').value = listUsers[index].phone;
+    document.getElementById('country').value = listUsers[index].country;
+    document.getElementById('city').value = listUsers[index].city;
+    document.getElementById('address').value = listUsers[index].address;
+
+    document.getElementById('update').onclick = function(){
+    if(validateForm() == true) {
+        listUsers[index].lastName = document.getElementById('lastName').value;
+        listUsers[index].firstName = document.getElementById('firstName').value;
+        listUsers[index].email = document.getElementById('email').value;
+        listUsers[index].phone = document.getElementById('phone').value;
+        listUsers[index].country = document.getElementById('country').value;
+        listUsers[index].city = document.getElementById('city').value;
+        listUsers[index].address = document.getElementById('address').value;
+
+        localStorage.setItem('listUsers', JSON.stringify(listUsers));
+        showUsers();
+
+        location.reload();
+    }
+      
+    }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,16 @@ var add = document.querySelector('.btn-add');
 var formAdd = document.querySelector('.form-add');
 var closeModal = document.querySelector('.close-modal');
 var overflow = document.querySelector('.overflow');
-var btnUpdate = document.querySelector('.btn-update');
+var btnUpdate = document.querySelectorAll('.btn-update');
     add.addEventListener('click', function(){
         overflow.classList.add('active');
     });
-    btnUpdate.addEventListener('click', function(){
-        overflow.classList.add('active');
+    btnUpdate.forEach(element => {
+        element.addEventListener('click', function(){
+            overflow.classList.add('active');
+        });
     });
+    
     closeModal.addEventListener('click', function(){
         overflow.classList.remove('active');
     });
@@ -18,4 +21,37 @@ var btnUpdate = document.querySelector('.btn-update');
     formAdd.addEventListener('click', function(e){
         e.stopPropagation();
     });
-   
+
+
+
+    document.querySelector('.cancel').onclick = function() {
+        document.querySelector('.confirm-delete').style.display = 'none';
+    }
+    // var cancel = document.querySelector('.cancel');
+
+    // Confirm delete
+    // var Delete = document.querySelectorAll('.btn-delete');
+    // var modal = document.querySelector('.modal');
+    // var cancel = document.querySelector('.cancel');
+    // var confirmDelete = document.querySelector('.confirm-delete');
+    // var btnDelete = document.querySelectorAll('.btn-delete');
+    // Delete.forEach(element => {
+    //     element.addEventListener('click', function(){
+    //         confirmDelete.classList.add('active');
+    //     });
+    // });
+    //     btnDelete.forEach(element => {
+    //         element.addEventListener('click', function(){
+    //             confirmDelete.classList.add('active');
+    //         });
+    //     });
+        
+    //     cancel.addEventListener('click', function(){
+    //         confirmDelete.classList.remove('active');
+    //     });
+    //     confirmDelete.addEventListener('click', function(){
+    //         confirmDelete.classList.remove('active');
+    //     });
+    //     modal.addEventListener('click', function(e){
+    //         e.stopPropagation();
+    //     });
