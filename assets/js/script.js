ff14cafe7caa829dd316d02161131cb463476179
Tuấnu <minhtuan21102004@an21102004gmail.com>
2024-08-28 10:35:34 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,37 +21,3 @@ var btnUpdate = document.querySelectorAll('.btn-update');
     formAdd.addEventListener('click', function(e){
         e.stopPropagation();
     });
-
-
-
-    document.querySelector('.cancel').onclick = function() {
-        document.querySelector('.confirm-delete').style.display = 'none';
-    }
-    // var cancel = document.querySelector('.cancel');
-
-    // Confirm delete
-    // var Delete = document.querySelectorAll('.btn-delete');
-    // var modal = document.querySelector('.modal');
-    // var cancel = document.querySelector('.cancel');
-    // var confirmDelete = document.querySelector('.confirm-delete');
-    // var btnDelete = document.querySelectorAll('.btn-delete');
-    // Delete.forEach(element => {
-    //     element.addEventListener('click', function(){
-    //         confirmDelete.classList.add('active');
-    //     });
-    // });
-    //     btnDelete.forEach(element => {
-    //         element.addEventListener('click', function(){
-    //             confirmDelete.classList.add('active');
-    //         });
-    //     });
-        
-    //     cancel.addEventListener('click', function(){
-    //         confirmDelete.classList.remove('active');
-    //     });
-    //     confirmDelete.addEventListener('click', function(){
-    //         confirmDelete.classList.remove('active');
-    //     });
-    //     modal.addEventListener('click', function(e){
-    //         e.stopPropagation();
-    //     });
