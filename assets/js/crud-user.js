51001d5ed67016c48dc173124f080265857cba51
Tuấnu <minhtuan21102004@an21102004gmail.com>
2024-08-29 10:08:33 +0700

    phân luồng

--- assets/js/crud-user.js
+++ assets/js/crud-user.js
@@ -1,80 +1,66 @@
-// validate form
-function validateForm(){
-    var lastName = document.getElementById('lastName').value;
-    var firstName = document.getElementById('firstName').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-    var country = document.getElementById('country').value;
-    var city = document.getElementById('city').value;
-    var address = document.getElementById('address').value;
+"use strict";
 
-    if(lastName == ""){
-        alert("Vui lòng nhập Họ");
-        return false;
-    }
-    if(firstName == ""){
-        alert("Vui lòng nhập Tên");
-        return false;
-    }
-    if(email == ""){
-        alert("Vui lòng nhập Email");
-        return false;
-    }
-    if(phone == ""){
-        alert("Vui lòng nhập phone");
-        return false;
-    }
-    if(country == ""){
-        alert("Vui lòng nhập country");
-        return false;
-    }
-    if(city == ""){
-        alert("Vui lòng nhập city");
-        return false;
-    }
-    if(address == ""){
-        alert("Vui lòng nhập address");
-        return false;
-    }
+const STATE_USER = {
+    state_users: [],
+
+    //Khởi tạo dữ liệu từ LocalStorage
+    init: function(){
+        const storedUsers = localStorage.getItem('LIST_INFO');
+        this.state_users = storedUsers ? JSON.parse(storedUsers) : [];
+        this.renderUserList();
+    },
+
+    // lưu danh sách dữ liệu vào LocalStorage
+    saveUsers: function(){
+        localStorage.setItem('LIST_INFO', JSON.stringify(this.state_users));
+    },
+
+    //Thêm User
+    addUser: function(user){
+        this.state_users.push(user);
+        this.saveUsers();
+        this.renderUserList();
+    },
+
+    //Sửa User
+    editUser: function(id, updateUser){
+        this.state_users[id] = updateUser;
+        this.saveUsers();
+        this.renderUserList();
+    },
+
+    //Xóa User
+    deleteUser: function(id){
+        this.state_users.splice(id, 1);
+        this.saveUsers();
+        this.renderUserList();
+    },
     
-    return true;
-}
-
-// Show users
-function showUsers(){
-    var listUsers;
-    if(localStorage.getItem("listUsers") == null){
-        listUsers = []; 
-    }else{
-        listUsers = JSON.parse(localStorage.getItem("listUsers"));
-    }
-
-    var html = "";
-    listUsers.forEach(function(element, index) {
-        html += `
-            <tr>
-                    <td>${element.lastName} ${element.firstName}</td>
-                    <td>${element.email}</td>
-                    <td>${element.address}</td>
-                    <td>${element.city}</td>
-                    <td>${element.country}</td>
-                    <td class="action">
-                        <a href="${index}"><i class="fa-regular fa-eye"></i></a> |
-                        <button onclick="updateUser(${index})" class="btn-update"><i class="fa-regular fa-pen-to-square"></i></button> |
-                        <button onclick="confirmDelete(${index})" class="btn-delete"><i class="fa-regular fa-trash-can"></i></button>
-                    </td>
-                </tr>
-        `
-    });
-    document.querySelector("#list-users tbody").innerHTML = html;
-}
-
-
-document.onload = showUsers();
-
-// Add user
-function addUser(){
-    if(validateForm() == true){
+    // Hiển thị danh sách người dùng
+    renderUserList: function(){
+        const userListContainer = document.querySelector('#list-users tbody');
+            userListContainer.innerHTML = '';
+            this.state_users.forEach((user, index) => {
+                userListContainer.innerHTML += `
+                    <tr>
+                        <td>${user.lastName} ${user.firstName}</td>
+                        <td>${user.email}</td>
+                        <td>${user.address}</td>
+                        <td>${user.city}</td>
+                        <td>${user.country}</td>
+                        <td class="action">
+                            <a href="${index}"><i class="fa-regular fa-eye"></i></a> |
+                            <button class="btn-update"><i class="fa-regular fa-pen-to-square"></i></button> |
+                            <button class="btn-delete" data-index="${index}"><i class="fa-regular fa-trash-can"></i></button>
+                        </td>
+                    </tr>
+                `;
+            });
+            listenDeleteUser();
+    },
+    
+    //Xử lý form 
+    handleFormSubmit:  function() {
         var lastName = document.getElementById('lastName').value;
         var firstName = document.getElementById('firstName').value;
         var email = document.getElementById('email').value;
@@ -83,24 +69,10 @@ function addUser(){
         var city = document.getElementById('city').value;
         var address = document.getElementById('address').value;
 
-        var listUsers;
-        if(localStorage.getItem("listUsers") == null){
-            listUsers = []; 
-        }else{
-            listUsers = JSON.parse(localStorage.getItem("listUsers"));
-        }
-        listUsers.push({
-            lastName: lastName,
-            firstName: firstName,
-            email: email,
-            phone: phone,
-            country: country,
-            city: city,
-            address: address
-        })
-
-        localStorage.setItem('listUsers', JSON.stringify(listUsers));
-        showUsers();
+        const user = {lastName, firstName, email, phone, country, city, address}
+      
+        this.addUser(user);
+    
         var lastName = document.getElementById('lastName').value = "";
         var firstName = document.getElementById('firstName').value = "";
         var email = document.getElementById('email').value = "";
@@ -108,73 +80,68 @@ function addUser(){
         var country = document.getElementById('country').value = "";
         var city = document.getElementById('city').value = "";
         var address = document.getElementById('address').value = "";
-
-    location.reload();
-
     }
+};
 
+//Sự kiện
+function listenNewUser(){
+    var form = document.getElementById('save');
+    form.addEventListener('click', function(e){
+        e.preventDefault();
+        STATE_USER.handleFormSubmit();
+    });
+}
+function listenDeleteUser(){
+    const btnDelete = document.querySelectorAll('.btn-delete');
+    btnDelete.forEach(function(item){
+        item.addEventListener('click', function(){
+                const index = this.getAttribute('data-index');
+                STATE_USER.deleteUser(index);        
+        })
+    });
+    
 }
 
-function confirmDelete(index) {
-    document.querySelector('.confirm-delete').classList.add('active');
-    document.querySelector('.delete').setAttribute('onclick', `deleteUser(${index})`);
-    document.querySelector('.cancel').onclick = function() {
-        document.querySelector('.confirm-delete').classList.remove('active');
-    }
+function getUsers() {
+    return STATE_USER.users;
+}
+function listUsers(){
+    STATE_USER.renderUserList();
+    listenDeleteUser();
+}
+function createUser(data){
+    STATE_USER.addUser(data);
 }
 
-// Delete user
-function deleteUser(index){
-    var listUsers;
-    if(localStorage.getItem("listUsers") == null){
-        listUsers = []; 
-    }else{
-        listUsers = JSON.parse(localStorage.getItem("listUsers"));
-    }
 
-    listUsers.splice(index, 1);
-    localStorage.setItem('listUsers', JSON.stringify(listUsers));
-    document.querySelector('.confirm-delete').style.display = 'none';
-    showUsers();
-    location.reload();
-
+//Hàm chạy chính
+function main() {
+    STATE_USER.init();
+    listenNewUser();
+    listUsers();
 }
+main();
 
-//update and edit
-function updateUser(index){
-    document.getElementById('save').style.display = 'none';
-    document.getElementById('title-add').style.display = 'none';
-    document.getElementById('update').style.display = 'block';
-    document.getElementById('title-update').style.display = 'block';
-    var listUsers;
-        if(localStorage.getItem("listUsers") == null){
-            listUsers = []; 
-        }else{
-            listUsers = JSON.parse(localStorage.getItem("listUsers"));
-        }
-    document.getElementById('lastName').value = listUsers[index].lastName;
-    document.getElementById('firstName').value = listUsers[index].firstName;
-    document.getElementById('email').value = listUsers[index].email;
-    document.getElementById('phone').value = listUsers[index].phone;
-    document.getElementById('country').value = listUsers[index].country;
-    document.getElementById('city').value = listUsers[index].city;
-    document.getElementById('address').value = listUsers[index].address;
 
-    document.getElementById('update').onclick = function(){
-    if(validateForm() == true) {
-        listUsers[index].lastName = document.getElementById('lastName').value;
-        listUsers[index].firstName = document.getElementById('firstName').value;
-        listUsers[index].email = document.getElementById('email').value;
-        listUsers[index].phone = document.getElementById('phone').value;
-        listUsers[index].country = document.getElementById('country').value;
-        listUsers[index].city = document.getElementById('city').value;
-        listUsers[index].address = document.getElementById('address').value;
 
-        localStorage.setItem('listUsers', JSON.stringify(listUsers));
-        showUsers();
 
-        location.reload();
-    }
-      
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
